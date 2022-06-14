a5747d9878a12ea2de86f7efb480820eda6ca5e2
thebassettman <53096242+thebassettman@users.noreply.github.com>
2022-06-14 20:30:03 +0100

    Added validEntry
    Added method to check if a possible entry is valid.

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -55,12 +55,36 @@ def readSudoku():
 	print(problem)
 	return problem
 
+def validEntry(problem, i, j, entry):
+	for m in range(9):
+		if(problem[i][m][0] == entry):
+			return False
+	for n in range(9):
+		if(problem[n][j][0] == entry):
+			return False
+	
+	if(i >= 0 and i <= 2):
+		x = 0
+	if(i >= 3 and i <= 5):
+		x = 3
+	if(i >= 6 and i <= 9):
+		x = 6
+	if(j >= 0 and j <= 2):
+		y = 0
+	if(j >= 3 and j <= 5):
+		y = 3
+	if(j >= 6 and j <= 9):
+		y = 6
+
+	for a in range(x, x + 3):
+		for b in range(y, y + 3):
+			if(problem[a][b][0] == entry):
+				return False
+
+	return True
+
 problem = readSudoku()
 
-i, j = nextValue(problem, 5, 8)
-print(i)
-print(j)
-
-m, n = prevValue(problem, 2, 0)
-print(m)
-print(n)
+print(validEntry(problem, 0, 0, 2))
+print(validEntry(problem, 0, 0, 1))
+print(validEntry(problem, 0, 1, 7))
