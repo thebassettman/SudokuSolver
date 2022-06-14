e59478559f0aa43b8e17fe3b8370d0f9e6258d39
thebassettman <53096242+thebassettman@users.noreply.github.com>
2022-06-14 19:59:07 +0100

    Added nextValue
    Added method for finding next value that can be altered.
    
    Changed alterability flag to be stored as Boolean instead of string.

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -1,10 +1,30 @@
+def invalidProblem():
+	print("Problem has no valid solution.")
+	quit()
+
+def nextValue(problem, i, j):
+	found = False;
+	while(not found):
+		if(j == 8):
+			i += 1
+			j = 0
+		if(i > 8):
+			invalidProblem()
+		else:
+			j += 1
+
+		if(not problem[i][j][1]):
+			found = True;
+
+	return i, j
+
 def readSudokuLine(lineStr):
 	lineArr = []
 	for element in range(9):
 		if (lineStr[element] == 'x'):
-			lineArr.append([0, false])
+			lineArr.append([0, False])
 		else:
-			lineArr.append([lineStr[element], true])
+			lineArr.append([ord(lineStr[element]) - 48, True])
 	return lineArr
 
 def readSudoku():
@@ -17,5 +37,9 @@ def readSudoku():
 		problem.append(problemLineArr)
 
 	print(problem)
+	return problem
 
-readSudoku()
+problem = readSudoku()
+i, j = nextValue(problem, 6, 1)
+print(i)
+print(j)
